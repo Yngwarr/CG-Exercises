a5fb2234e25a9dfd46234a35c9c30e880bd15c78
GIRAFF <k.yngvarr@gmail.com>
2017-02-25 11:30:51 +0700

    Inital.

---
+++ lab1/src/main.cc
@@ -0,0 +1,134 @@
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+
+#define WIN_TITLE  "Первая лаба"
+
+struct point_t
+{
+	GLint x, y;
+	point_t() {}
+	point_t(GLint _x, GLint _y) : x(_x), y(_y) {}
+};
+
+struct color_t
+{
+	GLubyte r;
+	GLubyte g;
+	GLubyte b;
+	color_t(GLubyte _r, GLubyte _g, GLubyte _b) :
+		r(_r), g(_g), b(_b) {}
+	color_t invert()
+	{
+		color_t res(r ^ 0xFF, b ^ 0xFF, g ^ 0xFF);
+		return res;
+	}
+};
+
+GLint width = 800, height = 600;
+color_t color(0, 0, 0);
+
+std::vector<std::vector<point_t>> vs(1);
+
+void display()
+{
+	glClearColor(0.3, 0.3, 0.3, 1);
+	glClear(GL_COLOR_BUFFER_BIT);
+	//glPointSize(6);
+	glLineWidth(3);
+
+	if (vs.empty() || vs.back().empty()) goto q_display;
+		
+	glColor3ub(color.r, color.g, color.b);
+	for (auto ps : vs) {
+		glBegin(GL_POLYGON);
+		for (int i = 0; i < ps.size(); i++)
+			glVertex2i(ps[i].x, ps[i].y);
+		glEnd();
+	}
+
+	glColor3ub(color.r ^ 0xFF, color.g ^ 0xFF, color.b ^ 0xFF);
+	glBegin(GL_LINE_LOOP);
+	for (int i = 0; i < vs.back().size(); i++)
+		glVertex2i(vs.back()[i].x, vs.back()[i].y);
+	glEnd();
+
+q_display:
+	glFinish();
+}
+
+void reshape(GLint w, GLint h)
+{
+	width = w;
+	height = h;
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, w, 0, h);
+	//gluOrtho2D(0, w, h, 0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+	int i, n = vs.back().size();
+	/* Изменение RGB-компонент цвета точек */
+	if (key == 'r') color.r += 5;
+	if (key == 'g') color.g += 5;
+	if (key == 'b') color.b += 5;
+	if (key == 'R') color.r -= 5;
+	if (key == 'G') color.g -= 5;
+	if (key == 'B') color.b -= 5;
+	/* Изменение XY-кординат точек */
+	if (key == 'w') for (i = 0; i < n; i++) vs.back()[i].y += 9;
+	if (key == 's') for (i = 0; i < n; i++) vs.back()[i].y -= 9;
+	if (key == 'a') for (i = 0; i < n; i++) vs.back()[i].x -= 9;
+	if (key == 'd') for (i = 0; i < n; i++) vs.back()[i].x += 9;
+	glutPostRedisplay();
+}
+
+void mouse(int button, int state, int x, int y)
+{
+	/* клавиша была нажата, но не отпущена */
+	if (state != GLUT_DOWN) return;
+	/* новая точка по левому клику */
+	if (button == GLUT_LEFT_BUTTON)
+	{
+		point_t p(x, height - y);
+		vs.back().push_back(p);
+	}
+	/* удаление последней точки по правому клику */
+	if (button == GLUT_RIGHT_BUTTON) {
+		if (!vs.empty() && vs.back().empty()) {
+			vs.pop_back();
+			return;
+		}
+		vs.back().pop_back();
+	}
+	if (button == GLUT_MIDDLE_BUTTON) {
+		vs.push_back(std::vector<point_t>(0));
+	}
+	glutPostRedisplay();
+}
+
+int main(int argc, char **argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB);
+	glutInitWindowSize(width, height);
+	glutCreateWindow(WIN_TITLE);
+	glEnable(GL_POINT_SMOOTH);
+
+	glutDisplayFunc(display);
+	glutKeyboardFunc(keyboard);
+	glutReshapeFunc(reshape);
+	glutMouseFunc(mouse);
+
+	glutMainLoop();
+	return 0;
+}
